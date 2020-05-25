a8af1fbd31033d24aeac671170ff16500c3fe23b
Nitish Prasad <nk8713694@gmail.com>
2020-05-25 06:06:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// intervalid = setInterval(function () {
+
+//     count++;
+//     if (count > 5) {
+//         clearInterval(intervalid);
+//     }
+
+//     console.log("hello")
+// },
+//     1000)
+
+
+
+
+
+
