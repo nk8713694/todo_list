476c21be507013c4620976aa05583522e886e0e4
Nitish Prasad <nk8713694@gmail.com>
2020-05-28 13:45:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,81 @@
+// console.log("hello brow how are u i");
+
+let  ulTasks=$('#ulTasks')
+
+let  btnAdd=$('#btnAdd');
+
+let  btnReset=$('#btnReset');
+
+let btnSort=$('#btnSort');
+
+let btnCleanup=$('#btnCleanup');
+
+let  inpNewTask=$('#inpNewTask');
 
 
 
+function toggleResetBtn(enabled)
+{
+    if(enabled) btnReset.prop('disabled',false)
+    else
+    btnReset.prop('disabled',true);
+}
+inpNewTask.on('input',()=>
+{
+    console.log(inpNewTask.val());
+    
+   toggleResetBtn(inpNewTask.val() !="")
+})
 
 
+function addItem()
+{
+    let listItem=$('<li>',
+    {
+        'class':'list-group-item',
+        text:inpNewTask.val()
+    })
+
+    listItem.click(()=>
+    {
+        $(listItem).toggleClass('done')
+    })
+
+     ulTasks.append(listItem)
 
 
+    inpNewTask.val('')
+}
+
+inpNewTask.keypress((e)=>{
+
+    if(e.which==13)
+    {
+      addItem();
+    }
+})
+
+btnAdd.click(addItem);
 
 
+btnReset.click(()=>
+{
+    inpNewTask.val('')
+})
 
 
+function CleanDone()
+{
+  $('#ulTasks .done').remove();
+}
+btnCleanup.click(CleanDone);
 
+function sortTasks()
+{
+    $('#ulTasks .done').appendTo(ulTasks);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// intervalid = setInterval(function () {
-
-//     count++;
-//     if (count > 5) {
-//         clearInterval(intervalid);
-//     }
-
-//     console.log("hello")
-// },
-//     1000)
-
-
-
-
-
-
+btnSort.click(()=>
+{
+    $('#ulTasks .done').appendTo(ulTasks);
+});
